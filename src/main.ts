3e691f86c5b262cc97625c52c7b4aaea6a540b5b
An-Ran Lee <alee208@ucsc.edu>
2023-10-30 09:48:40 -0700

    first commit

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import "crisp-game-lib";
+
+const title = "";
+
+const description = `
+`;
+
+const characters: any = [];
+
+function update() {
+  if (!ticks) {
+  }
+}
+
+init({
+  update,
+  title,
+  description,
+  characters,
+  options: {},
+});
